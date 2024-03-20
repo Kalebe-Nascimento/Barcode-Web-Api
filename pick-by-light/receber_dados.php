bf2e9c7e032047678b97b440ba390dd340b3953e
Kalebe Nascimento <38252510+Kalebe-Nascimento@users.noreply.github.com>
2024-03-20 16:48:05 -0300

    Update receber_dados.php

--- pick-by-light/receber_dados.php
+++ pick-by-light/receber_dados.php
@@ -1,15 +1,15 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") //verifica se é um metodo POST{
     if (isset($_POST['barcode']) && isset($_POST['added_time']) && isset($_POST['modified_time']) && isset($_POST['title']) && isset($_POST['description']) && isset($_POST['brand']) && isset($_POST['manufacturing'])) {
-        $db = new SQLite3('dados.db');
+        $db = new SQLite3('dados.db'); //caso seja e verifica se POST de requisição feito pelo RASP fornece todos os dados, caso sim cria um BD dados
 
         if (!$db) {
             die("Erro ao conectar ao banco de dados.");
         }
 
-        $existingBarcode = $db->querySingle("SELECT COUNT(*) FROM Product WHERE barcode = '{$_POST['barcode']}'");
+        $existingBarcode = $db->querySingle("SELECT COUNT(*) FROM Product WHERE barcode = '{$_POST['barcode']}'"); //verifica se já foi registado
 
-        if ($existingBarcode == 0) {
+        if ($existingBarcode == 0) //caso não inciar um novo produto na tabela{
             $stmt = $db->prepare('INSERT INTO Product (barcode, added_time, modified_time, title, description, brand, manufacturing) VALUES (:barcode, :added_time, :modified_time, :title, :description, :brand, :manufacturing)');
 
             $stmt->bindValue(':barcode', $_POST['barcode']);
@@ -27,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             } else {
                 echo "Erro ao inserir os dados.";
             }
-        } else {
+        } else // caso sim, soma no contador de dados {
             $db->exec("UPDATE Product SET quantity = quantity + 1 WHERE barcode = '{$_POST['barcode']}'");
             echo "Quantidade do produto incrementada.";
         }
